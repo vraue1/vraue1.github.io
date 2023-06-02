0bc0f746dc359ad64727426ef77d2e74df1d2734
vraue1 <127079254+vraue1@users.noreply.github.com>
2023-06-02 13:23:52 +1000

    Add files via upload

---
+++ coding/boilerplate/a3/script.js
@@ -0,0 +1,118 @@
+
+let squareElement = document.getElementById("loading-container-2");
+
+var circleElement = document.querySelector(".circle");
+
+circleElement.addEventListener("click", function() {
+  circleElement.classList.add("loading");
+  squareElement.style.display="flex";
+  circleElement.style.display="none";
+});
+
+let throboneElement = document.getElementById("load-throbber-1");
+
+
+squareElement.addEventListener("click", function() {
+  squareElement.classList.add("loading");
+  throboneElement.style.display="flex";
+  squareElement.style.display="none";
+});
+
+
+let throbtwoElement = document.getElementById("load-throbber-2");
+
+var redElement = document.querySelector(".red");
+
+redElement.addEventListener("click", function() {
+  redElement.classList.add("loading");
+  throbtwoElement.style.display="flex";
+  redElement.style.display="none";
+});
+
+
+let throbthreeElement = document.getElementById("load-throbber-3");
+
+var pinkElement = document.querySelector(".pink");
+
+pinkElement.addEventListener("click", function() {
+  pinkElement.classList.add("loading");
+  throbthreeElement.style.display="flex";
+  pinkElement.style.display="none";
+});
+
+
+let throbfourElement = document.getElementById("load-throbber-4");
+
+var blueElement = document.querySelector(".blue");
+
+blueElement.addEventListener("click", function() {
+  blueElement.classList.add("loading");
+  throbfourElement.style.display="flex";
+  blueElement.style.display="none";
+});
+
+
+let throbfiveElement = document.getElementById("load-throbber-5");
+
+var purpleElement = document.querySelector(".purple");
+
+purpleElement.addEventListener("click", function() {
+  purpleElement.classList.add("loading");
+  throbfiveElement.style.display="flex";
+  purpleElement.style.display="none";
+});
+
+
+let throbsixElement = document.getElementById("load-throbber-6");
+
+var orangeElement = document.querySelector(".orange");
+
+orangeElement.addEventListener("click", function() {
+  orangeElement.classList.add("loading");
+  throbsixElement.style.display="flex";
+  orangeElement.style.display="none";
+});
+
+
+let throbsevenElement = document.getElementById("load-throbber-7");
+
+var greenElement = document.querySelector(".green");
+
+greenElement.addEventListener("click", function() {
+  greenElement.classList.add("loading");
+  throbsevenElement.style.display="flex";
+  greenElement.style.display="none";
+});
+
+
+let throbeightElement = document.getElementById("load-throbber-8");
+
+var yellowElement = document.querySelector(".yellow");
+
+yellowElement.addEventListener("click", function() {
+  yellowElement.classList.add("loading");
+  throbeightElement.style.display="flex";
+  yellowElement.style.display="none";
+});
+
+
+let throbnineElement = document.getElementById("load-throbber-9");
+
+var whiteElement = document.querySelector(".white");
+
+whiteElement.addEventListener("click", function() {
+  whiteElement.classList.add("loading");
+  throbnineElement.style.display="flex";
+  whiteElement.style.display="none";
+});
+
+
+let throbtenElement = document.getElementById("load-throbber-10");
+
+var greyElement = document.querySelector(".grey");
+
+greyElement.addEventListener("click", function() {
+  greyElement.classList.add("loading");
+  throbtenElement.style.display="flex";
+  greyElement.style.display="none";
+});
